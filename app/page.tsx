8e76cbd2cd16accba2a3ef713f5641d94bc5cd8e
hemankkumar24 <hemankkumar24@gmail.com>
2026-03-19 00:43:07 +0530

    update: more updates on landing page:

--- app/page.tsx
+++ app/page.tsx
@@ -5,16 +5,16 @@ const page = () => {
   return (
     <div className='w-full bg-stone-50'>
 
-      {/* Navbar */}
+      {/* Navbar Section */}
       <div className='w-full px-10 py-5 border-b-stone-500 border relative'>
         <div className='w-full flex justify-between items-center'>
           {/* Left Buttons and Right Buttons */}
           <div className='main-font text-lg'>
             Home
           </div>
-          <div className='main-font text-lg'>
+          <button className='main-font text-lg'>
             Create Account
-          </div>
+          </button>
         </div>
         {/* Middle Name */}
         <div className="absolute inset-0 flex items-start justify-center pt-5 main-font text-2xl text-blue-700">
@@ -34,7 +34,7 @@ const page = () => {
           <div className="absolute bottom-0 left-1/2 -translate-x-1/2 h-2 w-10 bg-stone-600 pointer-events-none"></div>
         </div>
       </div>
-      {/* Hero Text */}
+      {/* Hero Text Section */}
       <div className='py-10 flex flex-col items-center border-b-stone-500 border-b'>
         <div className='text-8xl px-25 text-center main-font text-stone-800'>
           Beyond tracking: clearer, smarter, and
@@ -44,7 +44,7 @@ const page = () => {
           Track expenses, understand your spending, and make confident financial decisions all in one place.
         </div>
       </div>
-      {/* Phone */}
+      {/* Phone Section */}
       <div className='py-10 px-10 flex justify-between items-center main-font w-full h-[60vh] border-b-stone-500 border-b'>
         <div className=''>
           <div className='px-3 py-1 bg-stone-100 border-stone-200 border inline-block rounded-2xl text-stone-500 font-bold'>
@@ -67,7 +67,7 @@ const page = () => {
       </div>
 
       {/* Skill Section */}
-      <div className="py-2 border-b-stone-500 border-b text-3xl main-font gap-5 justify-center flex whitespace-nowrap">
+      <div className="py-2 border-b-stone-500 border-b text-3xl main-font gap-5 justify-center flex overflow-x-hidden whitespace-nowrap">
         <span>Real-time insights</span>
         <span className='text-blue-700'>•</span>
         <span>Track every expense</span>
@@ -83,7 +83,7 @@ const page = () => {
         <span>Analyze effortlessly</span>
       </div>
 
-      {/* Description */}
+      {/* Description Section */}
       <div className='py-25 border-b border-b-stone-500'>
         <div className='main-font text-5xl px-10'>
           <div className='text-stone-800'>
@@ -95,7 +95,8 @@ const page = () => {
         </div>
       </div>
 
-      <div className='grid grid-cols-2'>
+      {/* FAQ Section */}
+      <div className='grid grid-cols-2 border-b border-b-stone-500'>
         <div className='p-10 main-font text-6xl border-r-stone-500 border-r'>
           FAQ
         </div>
@@ -123,6 +124,29 @@ const page = () => {
           <FAQItem title="Can I use this across devices?">
             Yes. You can use Dashance on your phone, laptop, or tablet. Your data is linked to your account and stays in sync across all your devices.
           </FAQItem>
+        </div>
+      </div>
+
+      {/* Get Started Section */}
+      <div className='main-font'>
+        <div className='bg-stone-900 relative w-full h-full pt-40 pb-140 flex flex-col items-center text-center overflow-hidden'>
+          <div className='text-6xl text-stone-100'>
+            Start managing your money with <span className='italic-font'>clarity</span>
+          </div>  
+          <div className='pt-5 text-3xl text-stone-400 w-[60%]'>
+            Simple, private, and built for control. Everything you need to track, understand, and improve your finances.
+          </div>
+          <div className='pt-5 flex gap-5'>
+            <button className='px-10 py-2 text-xl border border-stone-300 font-bold bg-white inline-block rounded-4xl text-stone-500 font-main'>
+              Login
+            </button>
+            <button className='text-stone-100 text-xl'>
+              Get Started 
+            </button>
+          </div>
+          <div className="absolute text-[420px] bottom-0 left-1/2 -translate-x-1/2 translate-y-3/10 text-stone-200/50 italic-font">
+            Dashance
+          </div>
         </div>
       </div>
     </div>
