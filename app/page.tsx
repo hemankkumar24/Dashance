f49810baa33becde8487227941ea2f445dfef768
hemankkumar24 <hemankkumar24@gmail.com>
2026-03-20 00:46:11 +0530

    update: more updates

--- app/page.tsx
+++ app/page.tsx
@@ -55,18 +55,38 @@ const page = () => {
     const words = gsap.utils.toArray<HTMLElement>(".word");
 
     // Words Being Seen Lock
-    gsap.to(words, {
-      opacity: 1,
-      color: "#78716c",
-      stagger: 0.1,
-      ease: "none",
-      scrollTrigger: {
-        trigger: ".word-section",
-        start: "top top",
-        end: "+=1000",
-        scrub: 1,
-        pin: true,
-      }
+    const mm = gsap.matchMedia();
+
+    mm.add("(min-width: 900px)", () => {
+      gsap.to(words, {
+        opacity: 1,
+        color: "#78716c",
+        stagger: 0.1,
+        ease: "none",
+        scrollTrigger: {
+          trigger: ".word-section",
+          start: "top top",
+          end: "bottom top", // 🔥 ends when section leaves viewport
+          scrub: 1,
+          pin: true,
+        }
+      });
+    });
+
+    mm.add("(max-width: 899px)", () => {
+      gsap.to(words, {
+        opacity: 1,
+        color: "#78716c",
+        stagger: 0.1,
+        ease: "none",
+        scrollTrigger: {
+          trigger: ".word-section",
+          start: "top 50%",
+          end: "bottom 50%", // same here
+          scrub: 1,
+          markers: true
+        }
+      });
     });
 
     // animation for more in control text
@@ -111,18 +131,21 @@ const page = () => {
     <div className='w-full bg-stone-50'>
 
       {/* Navbar Section */}
-      <div className='w-full px-10 py-5 border-b-stone-500 border relative'>
+      <div className='w-full px-5 lg:px-10 py-5 border-b-stone-500 border relative'>
         <div className='w-full flex justify-between items-center'>
           {/* Left Buttons and Right Buttons */}
-          <div className='main-font text-lg'>
+          <div className='main-font hidden xl:block text-lg'>
             Home
+          </div>
+          <div className="main-font text-2xl pointer-events-none block xl:hidden">
+            Dashance
           </div>
           <button className='main-font text-lg'>
             <Link href="/signup">Create Account</Link>
           </button>
         </div>
         {/* Middle Name */}
-        <div className="absolute inset-0 flex items-start justify-center pt-5 main-font text-2xl pointer-events-none">
+        <div className="absolute inset-0 xl:flex items-start justify-center pt-5 main-font text-2xl pointer-events-none hidden">
           Dashance
         </div>
       </div>
@@ -141,10 +164,10 @@ const page = () => {
       </div>
       {/* Hero Text Section */}
       <div className='py-10 flex flex-col items-center border-b-stone-500 border-b'>
-        <div className='text-8xl animate-show-up px-25 text-center main-font text-stone-800 opacity-0'>
+        <div className='text-6xl md:text-8xl animate-show-up px-5 lg:px-25 text-center main-font text-stone-800 opacity-0'>
           Beyond tracking: clearer, smarter, and
           {/* More in control Animation */}
-          <span className="hover-text inline-block cursor-pointer italic-font">
+          <span className="hover-text hidden md:inline-block cursor-pointer italic-font">
             {Array.from(" more in control").map((char, i) => (
               <span key={i} className="letter inline-block relative overflow-hidden">
 
@@ -163,21 +186,22 @@ const page = () => {
               </span>
             ))}
           </span>
+          <span className='inline-block md:hidden text-6xl italic-font px-5'>more in control</span>
         </div>
-        <div className='text-xl main-font text-stone-500 pt-5'>
+        <div className='text-xl main-font text-stone-500 px-5 lg:px-0 pt-5 text-center'>
           Track expenses, understand your spending, and make confident financial decisions all in one place.
         </div>
       </div>
       {/* Phone Section */}
-      <div className='py-10 px-10 flex justify-between items-center main-font w-full h-[60vh] border-b-stone-500 border-b'>
+      <div className='py-20 md:py-10 px-5 lg:px-10 flex justify-between items-center main-font w-full md:h-[60vh] border-b-stone-500 border-b'>
         <div className=''>
           <div className='px-3 py-1 bg-stone-100 border-stone-200 border inline-block rounded-2xl text-stone-500 font-bold'>
             Smooth Onboarding
           </div>
-          <div className='text-5xl pt-5 w-[70%]'>
+          <div className='text-5xl pt-5 md:w-[70%]'>
             Your Personal finances, a few taps away
           </div>
-          <div className='text-xl text-stone-500 pt-5 w-[70%]'>
+          <div className='text-xl text-stone-500 pt-5 md:w-[70%]'>
             Track, manage, and grow your money effortlessly.
             From daily expenses to long-term goals everything in one place.
           </div>
@@ -225,7 +249,7 @@ const page = () => {
 
       {/* Description Section */}
       <div className="word-section py-35 border-b border-b-stone-500 overflow-hidden">
-        <div className='main-font text-5xl px-10'>
+        <div className='main-font text-5xl px-5 lg:px-10'>
           <div className='text-stone-800 animate-show-up'>
             Private by design. Powerful by default.
           </div>
@@ -241,10 +265,10 @@ const page = () => {
 
       {/* FAQ Section */}
       <div className='grid grid-cols-2 border-b border-b-stone-500'>
-        <div className='p-10 main-font text-6xl border-r-stone-500 border-r animate-show-up'>
+        <div className='p-5 lg:p-10 main-font text-6xl border-r-stone-500 border-r animate-show-up'>
           FAQ
         </div>
-        <div className='p-10 flex flex-col gap-5 main-font mb-6'>
+        <div className='p-5 lg:p-10 flex flex-col gap-5 main-font mb-6'>
           <div className='animate-show-from-left'>
             <FAQItem title="What can I do with Dashance?">
               Easily track your expenses, understand where your money goes, and set simple financial goals. Everything is designed to give you clarity without complexity.
@@ -285,11 +309,11 @@ const page = () => {
 
       {/* Get Started Section */}
       <div className='main-font'>
-        <div className='bg-stone-900 relative w-full h-full pt-40 pb-140 flex flex-col items-center text-center overflow-hidden'>
-          <div className='text-6xl text-stone-100 animate-show-up'>
+        <div className='bg-stone-900 relative w-full h-full pt-40 pb-60 lg:pb-140 flex flex-col items-center text-center overflow-hidden'>
+          <div className='text-6xl px-5 text-stone-100 animate-show-up'>
             Start managing your money with <span className='italic-font'>clarity</span>
           </div>
-          <div className='pt-5 text-3xl text-stone-400 w-[60%]'>
+          <div className='pt-5 text-3xl text-stone-400 lg:w-[60%]'>
             Simple, private, and built for control. Everything you need to track, understand, and improve your finances.
           </div>
           <div className='pt-5 flex gap-5'>
@@ -308,7 +332,7 @@ const page = () => {
               <Link href="/signup">Get Started</Link>
             </button>
           </div>
-          <div className="absolute text-[420px] bottom-0 left-1/2 -translate-x-1/2 translate-y-3/10 text-stone-200/30 italic-font">
+          <div className="absolute text-[25vw] bottom-0 left-1/2 -translate-x-1/2 translate-y-3/10 text-stone-200/30 italic-font">
             Dashance
           </div>
         </div>
