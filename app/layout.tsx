fcbfec12b0871b07c61ba043f6969df318e4efb7
hemankkumar24 <hemankkumar24@gmail.com>
2026-03-19 12:01:40 +0530

    verification: google indexing

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,11 @@ import { cn } from "@/lib/utils";
 
 const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
+export const metadata = {
+  verification: {
+    google: "3HjCeOD9Wj1On6UcSk3_rrB2IK0Re6hMeVY2TXb3j8w",
+  },
+};
 
 export default function RootLayout({
   children,
