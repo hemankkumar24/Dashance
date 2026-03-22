d401fd05acfaaf5ff13e8c1fa8b0c4864a83bc10
hemankkumar24 <hemankkumar24@gmail.com>
2026-03-22 19:26:45 +0530

    update: semi gauge and goal card completed

--- app/components/Dashboard/SemiGauge.tsx
+++ app/components/Dashboard/SemiGauge.tsx
@@ -5,50 +5,58 @@ interface Props {
     max: number
 }
 
-const SemiGauge = ({ progress = 30, value , max }: Props) => {
-  const radius = 70
+export const SemiGauge = ({ progress, value, max }: Props) => {
   const stroke = 12
-  const normalizedRadius = radius - stroke / 2
-  const circumference = Math.PI * normalizedRadius // half circle
+  const width = 200
+  const radius = width / 2 - stroke / 2
+  const height = radius + stroke
+  const circumference = Math.PI * radius
+  const clampedProgress = Math.min(100, Math.max(0, progress))
+  const offset = circumference * (1 - clampedProgress / 100)
 
-  const offset = circumference - (progress / 100) * circumference
+  const cx = width / 2
+  const cy = height - stroke / 2
+  const startX = cx - radius
+  const endX = cx + radius
+  const arcPath = `M ${startX} ${cy} A ${radius} ${radius} 0 0 1 ${endX} ${cy}`
 
 
   return (
     <div className="flex items-center justify-center w-full h-full">
-      <div className="relative w-64 h-40">
+      <div className="relative w-full max-w-xs aspect-2/1">
 
         <svg
-          viewBox="0 0 200 120"
+          viewBox={`0 0 ${width} ${height}`}
+          preserveAspectRatio="xMidYMid meet"
           className="w-full h-full"
         >
           {/* Background arc */}
           <path
-            d="M 20 100 A 80 80 0 0 1 180 100"
+            d={arcPath}
             fill="transparent"
             stroke="#e5e5e5"
-            strokeWidth="12"
+            strokeWidth={stroke}
             strokeLinecap="round"
           />
 
           {/* Progress arc */}
           <path
-            d="M 20 100 A 80 80 0 0 1 180 100"
+            d={arcPath}
             fill="transparent"
             stroke="#2563eb"
-            strokeWidth="12"
+            strokeWidth="13"
             strokeLinecap="round"
             strokeDasharray={circumference}
             strokeDashoffset={offset}
-            className="transition-all duration-500"
+            className="transition-[stroke-dashoffset] duration-700 ease-out"
           />
         </svg>
 
         {/* Center Content */}
-        <div className="absolute inset-0 flex flex-col items-center justify-center translate-y-4">
-          <div className="text-sm text-stone-400">Target</div>
-          <div className="text-3xl font-semibold text-white">${value}</div>
-          <div className="text-xs text-stone-400">/ ${max}</div>
+        <div className="absolute inset-0 top-10 flex flex-col items-center justify-center">
+          <div className="text-lg text-stone-400">Target</div>
+          <div className="text-5xl font-semibold text-white">${value}</div>
+          <div className="text-lg text-stone-400">/ ${max}</div>
         </div>
 
       </div>
--- app/dashboard/components/GoalCard.tsx
+++ app/dashboard/components/GoalCard.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import React, { useState } from 'react'
+import { SemiGauge } from "../../components/Dashboard/SemiGauge"
 
 interface Props {
     title: string,
@@ -10,7 +11,7 @@ interface Props {
 const GoalCard = ({ title, progress }: Props) => {
     const [toggled, setToggled] = useState(false)
     return (
-        <div className={`w-full py-5 ${toggled ? "bg-zinc-700 text-stone-100" : "bg-stone-200 text-stone-800"} shrink-0 rounded-xl px-5 main-font transition-all duration-300`}>
+        <div className={`w-full py-5 ${toggled ? "bg-stone-800 text-stone-100" : "bg-stone-200 text-stone-800"} shrink-0 rounded-xl px-5 main-font transition-all duration-300`}>
 
             <div className='h-full w-full flex items-center justify-around gap-4 '>
 
@@ -22,12 +23,12 @@ const GoalCard = ({ title, progress }: Props) => {
                 <div className='flex-1 flex gap-2 items-center'>
 
                     {/* Percentage */}
-                    <div className={`text-sm ${toggled ? "text-stone-300" : "text-stone-600"}`}>
+                    <div className={`text-sm ${toggled ? "opacity-0": "opacity-100"}  transition-opacity duration-200`}>
                         {progress}%
                     </div>
 
                     {/* Progress Bar */}
-                    <div className='w-[60%] h-3 bg-stone-300 rounded-full overflow-hidden'>
+                    <div className={`${toggled ? "opacity-0": "opacity-100"} transition-opacity duration-200 w-[60%] h-3 bg-stone-300 rounded-full overflow-hidden`}>
                         <div
                             className='h-full bg-blue-500 rounded-full transition-all duration-500'
                             style={{ width: `${progress}%` }}
@@ -37,56 +38,13 @@ const GoalCard = ({ title, progress }: Props) => {
                 </div>
 
                 {/* Action Icon */}
-                <div className='w-8 text-lg h-8 flex justify-center items-center text-stone-800 bg-stone-300 rounded-full cursor-pointer hover:bg-stone-400/50 select-none transition' onClick={(() => { setToggled(!toggled) })}>
+                <div className={`w-8 text-lg h-8 flex justify-center items-center text-stone-800 bg-stone-300 rounded-full cursor-pointer hover:bg-stone-400/50 select-none transition ${toggled && "rotate-180"} transition-transform duration-300`} onClick={(() => { setToggled(!toggled) })}>
                     v
                 </div>
             </div>
-            <div className={`overflow-hidden transition-all duration-300 ${toggled ? "max-h-70 mt-4" : "max-h-0"}`}>
-                <div className='h-70 flex items-center justify-center'>
-
-                    <div className="relative w-52 h-52">
-
-                        {/* SVG Arc */}
-                        <svg className="w-full h-full -rotate-90">
-
-                            {/* Background Arc */}
-                            <circle
-                                cx="100"
-                                cy="100"
-                                r="80"
-                                stroke="#e7e5e4"
-                                strokeWidth="12"
-                                fill="transparent"
-                                strokeDasharray="502"
-                                strokeDashoffset="125"
-                                strokeLinecap="round"
-                            />
-
-                            {/* Progress Arc */}
-                            <circle
-                                cx="100"
-                                cy="100"
-                                r="80"
-                                stroke="#2563eb"
-                                strokeWidth="13"
-                                fill="transparent"
-                                strokeDasharray="502"
-                                strokeDashoffset={502 - (502 * progress) / 100}
-                                strokeLinecap="round"
-                                className="transition-all duration-500"
-                            />
-
-                        </svg>
-
-                        {/* Center Content */}
-                        <div className="absolute inset-0 flex flex-col items-center justify-center">
-                            <div className="text-sm text-stone-500">Target</div>
-                            <div className="text-2xl font-semibold">${Math.floor(progress * 20)}</div>
-                            <div className="text-xs text-stone-400">/ $2000</div>
-                        </div>
-
-                    </div>
-
+            <div className={`overflow-hidden transition-all duration-500 ${toggled ? "max-h-70 mt-4" : "max-h-0"}`}>
+                <div className='h-60 flex items-center justify-center'>
+                    <SemiGauge progress={30} value={600} max={2000} />
                 </div>
             </div>
         </div>
