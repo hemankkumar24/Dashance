114aaca2e7e602d1b2d2109b495d9291f8b51dc9
hemankkumar24 <hemankkumar24@gmail.com>
2026-03-22 19:33:26 +0530

    fix: minor scrolling fix

--- app/dashboard/components/Goal.tsx
+++ app/dashboard/components/Goal.tsx
@@ -18,7 +18,10 @@ const Goal = () => {
       </div>
 
       {/* Scroll Area */}
-      <div className='px-2 py-2 mt-10 mb-10 flex-1 min-h-0 overflow-y-auto border-stone-200 rounded-lg border'>
+      <div
+        data-lenis-prevent
+        className='px-2 py-2 mt-10 mb-10 flex-1 min-h-0 overflow-y-auto border-stone-200 rounded-lg border'
+      >
         <div className='w-full rounded-lg flex flex-col bg-stone-100 gap-y-2 '>
           <GoalCard title="Lamborghini" progress={30} />
           <GoalCard title="Lamborghini" progress={10} />
