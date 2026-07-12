b81e0633d96fbdc59d6856a5110e749992cd9097
hemankkumar24 <hemankkumar24@gmail.com>
2026-07-12 23:36:09 +0530

    update: auth page fix ui for mobiles

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,7 +4,6 @@ import React, { useState } from 'react'
 import Link from 'next/link'
 import { LucideEye, LucideEyeOff } from 'lucide-react'
 
-
 const page = () => {
 
     const [toggled, setToggled] = useState(false)
@@ -12,23 +11,27 @@ const page = () => {
     return (
         <div className='w-full h-dvh bg-black'>
             <img
-                className='absolute w-full h-dvh  object-cover opacity-80 '
-                src="./images/wallpaper/wallpaper.jpg"
+                className='absolute w-full h-dvh object-cover opacity-80'
+                src="./images/wallpaper/wallpaperNew.webp"
             />
 
             <div className='flex justify-center items-center w-full h-full py-0 md:py-5 xl:py-0'>
-                
-                {/* Container */}
-                <div className='w-200 h-full xl:w-300 xl:h-200 xl:rounded-4xl md:border-stone-600 md:rounded-xl border xl:border-10'>
-                    
+                <div className='w-200 h-full xl:w-300 xl:h-200 xl:rounded-4xl md:border-gray-400 md:border-2 xl:border-stone-600 rounded-xl xl:border-10'>
+
                     <div className='xl:grid xl:grid-cols-2 w-full h-full'>
 
-                        {/* LEFT SIDE (hidden on small screens like signup) */}
-                        <div className='hidden xl:block bg-stone-950/20 rounded-2xl rounded-tr-none rounded-br-none backdrop-blur-xs relative'>
+                        {/* Left Side */}
+                        <div className='hidden xl:block bg-stone-950/30 rounded-2xl rounded-tr-none rounded-br-none backdrop-blur-xs relative'>
                             <div className='flex flex-col absolute bottom-0 left-0 px-5 py-5 gap-2'>
-                                <div className='italic-font text-6xl text-white'>Get</div>
-                                <div className='italic-font text-6xl text-white'>Everything</div>
-                                <div className='italic-font text-6xl text-white'>You Want</div>
+                                <div className='italic-font text-6xl text-white'>
+                                    Get
+                                </div>
+                                <div className='italic-font text-6xl text-white'>
+                                    Everything
+                                </div>
+                                <div className='italic-font text-6xl text-white'>
+                                    You Want
+                                </div>
 
                                 <div className='pt-5 main-font text-xl text-stone-100 w-[80%]'>
                                     You can get everything you want if you work hard, trust the process, and stick to the plan.
@@ -36,17 +39,16 @@ const page = () => {
                             </div>
                         </div>
 
-                        {/* RIGHT SIDE */}
-                        <div className='bg-stone-950/30 backdrop-blur-md xl:bg-stone-50 h-full relative rounded-none md:rounded-xl xl:rounded-none xl:rounded-tr-xl xl:rounded-br-xl'>
-                            
-                            {/* Logo */}
+                        {/* Right Side */}
+                        <div className='bg-stone-950/10 md:bg-stone-950/10 backdrop-blur-xs lg:backdrop-blur-sm xl:bg-stone-50 h-full relative rounded-none md:rounded-xl xl:rounded-none xl:rounded-tr-xl xl:rounded-br-xl'>
+
                             <div className='absolute inset-0 pt-5 text-2xl flex justify-center italic-font text-stone-50 xl:text-stone-800 pointer-events-none'>
                                 Dashance
                             </div>
 
                             {/* Content */}
                             <div className='flex flex-col justify-center gap-2 w-full h-full'>
-                                
+
                                 <div className='main-font text-stone-50 xl:text-stone-800 text-5xl w-full text-center'>
                                     Welcome Back
                                 </div>
@@ -55,26 +57,27 @@ const page = () => {
                                     Enter your email and password to access your account
                                 </div>
 
-                                <div className='px-4 md:px-10 pt-10'>
-                                    
+                                <div className='px-2 md:px-10 pt-10'>
+
                                     {/* Email */}
                                     <div>
                                         <div className='text-stone-50 xl:text-stone-800 text-lg'>
                                             Email
                                         </div>
+
                                         <input
                                             type="text"
-                                            className='text-lg w-full rounded-sm text-stone-500 px-3 py-3 bg-stone-100 outline-none border border-stone-300'
+                                            className='text-lg w-full rounded-sm text-stone-500 px-3 py-3 bg-stone-100 outline-none md:border-2 border-stone-300'
                                             placeholder="Enter your email"
                                         />
                                     </div>
 
                                     {/* Password */}
-                                    <div className='pt-5'>
-                                        
+                                    <div className='pt-3 xl:pt-5'>
                                         <div className='text-stone-50 xl:text-stone-800 text-lg'>
                                             Password
                                         </div>
+
                                         <div className="relative">
                                             <input
                                                 type={toggled ? "text" : "password"}
@@ -82,43 +85,54 @@ const page = () => {
                                                 placeholder="Enter your password"
                                             />
 
-                                            <div className='absolute top-1/2 right-3 -translate-y-1/2 cursor-pointer' onClick={() => {setToggled(!toggled)}}>
+                                            <div
+                                                className='absolute top-1/2 right-3 -translate-y-1/2 cursor-pointer'
+                                                onClick={() => setToggled(!toggled)}
+                                            >
                                                 {
-                                                    toggled ? <LucideEyeOff className='text-stone-500/50' /> : <LucideEye className='text-stone-500/50' />
+                                                    toggled
+                                                        ? <LucideEyeOff className='text-stone-500/50' />
+                                                        : <LucideEye className='text-stone-500/50' />
                                                 }
                                             </div>
                                         </div>
                                     </div>
 
                                     {/* Remember + Forgot */}
-                                    <div className='pt-2 flex justify-between text-sm text-stone-200 xl:text-stone-800'>
+                                    <div className='pt-3 xl:pt-5 flex justify-between text-sm text-stone-200 xl:text-stone-800'>
                                         <div className='flex gap-2'>
                                             <input type="checkbox" />
                                             Remember me
                                         </div>
+
                                         <div className='cursor-pointer'>
                                             Forgot Password
                                         </div>
                                     </div>
 
                                     {/* Button */}
-                                    <div className='pt-5'>
-                                        <button className="w-full px-3 py-3 bg-stone-800 text-stone-50 rounded-sm cursor-pointer active:bg-stone-400 hover:bg-stone-700 active:text-stone-800">
+                                    <div className='pt-3 xl:pt-5'>
+                                        <button className='w-full px-3 py-3 bg-stone-800 text-stone-50 rounded-sm cursor-pointer active:bg-stone-400 hover:bg-stone-700 active:text-stone-800'>
                                             Sign In
                                         </button>
                                     </div>
+
                                 </div>
                             </div>
 
-                            {/* Bottom link */}
+                            {/* Bottom Link */}
                             <div className='w-full absolute bottom-0 flex justify-center text-lg pb-2 gap-1'>
                                 <span className='text-stone-50 xl:text-stone-500'>
                                     Don't have an account?
                                 </span>
+
                                 <span className='text-stone-950 xl:text-stone-800'>
-                                    <Link href="/signup">Sign Up</Link>
+                                    <Link href="/signup">
+                                        Sign Up
+                                    </Link>
                                 </span>
                             </div>
+
                         </div>
                     </div>
                 </div>
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -11,12 +11,12 @@ const page = () => {
 
     return (
         <div className='w-full h-dvh bg-black'>
-            <img className='absolute w-full h-dvh object-cover opacity-80' src="./images/wallpaper/wallpaper.jpg"></img>
+            <img className='absolute w-full h-dvh object-cover opacity-80' src="./images/wallpaper/wallpaperNew.webp"></img>
             <div className='flex justify-center items-center w-full h-full py-0 md:py-5 xl:py-0'>
-                <div className='w-200 h-full xl:w-300 xl:h-200 xl:rounded-4xl md:border-stone-600 rounded-xl border xl:border-10'>
+                <div className='w-200 h-full xl:w-300 xl:h-200 xl:rounded-4xl md:border-gray-400 md:border-2 xl:border-stone-600 rounded-xl  xl:border-10'>
                     <div className='xl:grid xl:grid-cols-2 w-full h-full'>
                         {/* Left Side with Bottom Left Text */}
-                        <div className='hidden xl:block bg-stone-950/20 rounded-2xl rounded-tr-none rounded-br-none backdrop-blur-xs relative'>
+                        <div className='hidden xl:block bg-stone-950/30 rounded-2xl rounded-tr-none rounded-br-none backdrop-blur-xs relative'>
                             <div className='flex flex-col absolute bottom-0 left-0 px-5 py-5 gap-2'>
                                 <div className='italic-font text-6xl text-white'>
                                     Start
@@ -34,7 +34,7 @@ const page = () => {
                         </div>
 
                         {/* Right Side */}
-                        <div className='bg-stone-950/30 backdrop-blur-sm xl:bg-stone-50 h-full relative rounded-none md:rounded-xl xl:rounded-none xl:rounded-tr-xl xl:rounded-br-xl'>
+                        <div className='bg-stone-950/10 md:bg-stone-950/10 backdrop-blur-xs lg:backdrop-blur-sm xl:bg-stone-50 h-full relative rounded-none md:rounded-xl xl:rounded-none xl:rounded-tr-xl xl:rounded-br-xl'>
                             <div className='absolute inset-0 pt-5 text-2xl flex justify-center italic-font text-stone-50 xl:text-stone-800 pointer-events-none'>
                                 Dashance
                             </div>
@@ -48,15 +48,15 @@ const page = () => {
                                     Sign up to get started with your account
                                 </div>
 
-                                <div className='px-4 md:px-10 pt-10'>
+                                <div className='px-2 md:px-10 pt-10'>
                                     <div>
                                         <div className='text-stone-50 xl:text-stone-800 text-lg'>
                                             Email
                                         </div>
-                                        <input type="text" className='text-lg w-full rounded-sm text-stone-500 px-3 py-3 bg-stone-100 outline-none border border-stone-300' placeholder="Enter your email" />
+                                        <input type="text" className='text-lg w-full rounded-sm text-stone-500 px-3 py-3 bg-stone-100 outline-none md:border-2 border-stone-300' placeholder="Enter your email" />
                                     </div>
 
-                                    <div className='pt-5'>
+                                    <div className='pt-3 xl:pt-5'>
                                         <div className='text-stone-50 xl:text-stone-800 text-lg'>
                                             Password
                                         </div>
@@ -75,14 +75,14 @@ const page = () => {
                                         </div>
                                     </div>
 
-                                    <div className='pt-5'>
+                                    <div className='pt-3 xl:pt-5'>
                                         <div className='text-stone-50 xl:text-stone-800 text-lg'>
                                             Confirm Password
                                         </div>
                                         <input type="password" className='text-lg w-full rounded-sm text-stone-500 px-3 py-3 bg-stone-100 outline-none border border-stone-300' placeholder="Confirm your password" />
                                     </div>
 
-                                    <div className='pt-5'>
+                                    <div className='pt-3 xl:pt-5'>
                                         <button className="w-full px-3 py-3 bg-stone-800 text-stone-50 rounded-sm">
                                             Sign Up
                                         </button>
